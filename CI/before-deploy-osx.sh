40a3d4ea1c9b0e17c37d35c30be89e9a0d485f77
jp9000 <obs.jim@gmail.com>
2018-04-12 16:24:38 -0700

    Browser Builds test

--- CI/before-deploy-osx.sh
+++ CI/before-deploy-osx.sh
@@ -14,11 +14,6 @@ export FILENAME=$FILE_DATE-$GIT_HASH-$TRAVIS_BRANCH-osx.pkg
 
 cd ./build
 
-# Move the CEF plugin out before running build_app so that it doesn't get packaged twice
-hr "Moving CEF out to preserve linking"
-mv ./rundir/RelWithDebInfo/obs-plugins/CEF.app ./
-mv ./rundir/RelWithDebInfo/obs-plugins/obs-browser.so ./
-
 # Move obslua
 hr "Moving OBS LUA"
 mv ./rundir/RelWithDebInfo/data/obs-scripting/obslua.so ./rundir/RelWithDebInfo/bin/
@@ -37,10 +32,9 @@ fi
 
 sudo python ../CI/install/osx/build_app.py --public-key ../CI/install/osx/OBSPublicDSAKey.pem --sparkle-framework ../../sparkle/Sparkle.framework --base-url "https://obsproject.com/osx_update" --stable=$STABLE
 
-# Move the CEF plugin back to where it belongs
-hr "Moving CEF back"
-mv ./CEF.app ./rundir/RelWithDebInfo/obs-plugins/
-mv ./obs-browser.so ./rundir/RelWithDebInfo/obs-plugins/
+# Copy Chromium embedded framework to app Frameworks directory
+mkdir OBS.app/Contents/Frameworks
+sudo cp -r ../../cef_binary_${CEF_BUILD_VERSION}_macosx64/Release/Chromium\ Embedded\ Framework.framework OBS.app/Contents/Frameworks
 
 # Package app
 hr "Generating .pkg"
