f9c87f9f7dd7c31ca5385d62f1aa92b16627505b
jp9000 <obs.jim@gmail.com>
2018-04-12 17:59:21 -0700

    I'm sure I won't lose my mind any time soon testing this

--- CI/install-dependencies-osx.sh
+++ CI/install-dependencies-osx.sh
@@ -11,7 +11,7 @@ cd ../
 
 # Install Packages app so we can build a package later
 # http://s.sudre.free.fr/Software/Packages/about.html
-wget --retry-connrefused --waitretry=1 --quiet https://s3-us-west-2.amazonaws.com/obs-nightly/Packages.pkg
+wget --quiet --retry-connrefused --waitretry=1 https://s3-us-west-2.amazonaws.com/obs-nightly/Packages.pkg
 sudo installer -pkg ./Packages.pkg -target /
 
 brew update
@@ -23,21 +23,21 @@ export PATH=/usr/local/opt/ccache/libexec:$PATH
 ccache -s || echo "CCache is not available."
 
 # Fetch and untar prebuilt OBS deps that are compatible with older versions of OSX
-wget --retry-connrefused --waitretry=1 --quiet https://s3-us-west-2.amazonaws.com/obs-nightly/osx-deps.tar.gz
+wget --quiet --retry-connrefused --waitretry=1 https://s3-us-west-2.amazonaws.com/obs-nightly/osx-deps.tar.gz
 tar -xf ./osx-deps.tar.gz -C /tmp
 
 # Fetch vlc codebase
-wget --retry-connrefused --waitretry=1 -O --quiet vlc-master.zip https://github.com/videolan/vlc/archive/master.zip
+wget --quiet --retry-connrefused --waitretry=1 -O vlc-master.zip https://github.com/videolan/vlc/archive/master.zip
 unzip -q ./vlc-master.zip
 
 # Get sparkle
-wget --retry-connrefused --waitretry=1 -O --quiet sparkle.tar.bz2 https://github.com/sparkle-project/Sparkle/releases/download/1.16.0/Sparkle-1.16.0.tar.bz2
+wget --quiet --retry-connrefused --waitretry=1 -O sparkle.tar.bz2 https://github.com/sparkle-project/Sparkle/releases/download/1.16.0/Sparkle-1.16.0.tar.bz2
 mkdir ./sparkle
 tar -xf ./sparkle.tar.bz2 -C ./sparkle
 sudo cp -R ./sparkle/Sparkle.framework /Library/Frameworks/Sparkle.framework
 
 # CEF Stuff
-wget --retry-connrefused --waitretry=1 --quiet https://obs-nightly.s3-us-west-2.amazonaws.com/cef_binary_${CEF_BUILD_VERSION}_macosx64.tar.bz2
+wget --quiet --retry-connrefused --waitretry=1 https://obs-nightly.s3-us-west-2.amazonaws.com/cef_binary_${CEF_BUILD_VERSION}_macosx64.tar.bz2
 tar -xf ./cef_binary_${CEF_BUILD_VERSION}_macosx64.tar.bz2
 cd ./cef_binary_${CEF_BUILD_VERSION}_macosx64
 # remove a broken test
